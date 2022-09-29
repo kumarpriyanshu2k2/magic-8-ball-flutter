362f1b31eecc9c1a75f3a883aae63ebdb6dca9d9
kumarpriyanshu2k2 <kumar.priyanshu2k02@gmail.com>
2022-09-29 11:31:58 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/services.dart';
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Magic 8 Ball"),
+          ),
+          body: BallPage(),
+        )
+      ),
+);
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int bn =1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+               child:TextButton(
+                    onPressed: (){
+                      setState((){
+                        bn=Random().nextInt(5)+1;
+                      });
+
+                    },
+                   child: Image.asset("images/ball$bn.png")))
+
+        ],
       ),
     );
+  }
+}
